3c974bb8cfee98bab737455b7793bb4ba2ca7f7c
Matt Beaney <beaneyios@gmail.com>
2020-12-26 18:02:30 +0000

    Move crypto stuff into an extension.

--- Speezy/Auth/View Models/AppleSignupViewModel.swift
+++ Speezy/Auth/View Models/AppleSignupViewModel.swift
@@ -8,7 +8,6 @@
 
 import Foundation
 import AuthenticationServices
-import CryptoKit
 import FirebaseAuth
 
 class AppleSignupViewModel: NSObject {
@@ -22,61 +21,17 @@ class AppleSignupViewModel: NSObject {
     }
     
     func startSignInWithAppleFlow() {
-        let nonce = randomNonceString()
+        let nonce = String.nonce()
         currentNonce = nonce
         let appleIDProvider = ASAuthorizationAppleIDProvider()
         let request = appleIDProvider.createRequest()
         request.requestedScopes = [.fullName, .email]
-        request.nonce = sha256(nonce)
+        request.nonce = String.sha256(nonce)
         
         let authorizationController = ASAuthorizationController(authorizationRequests: [request])
         authorizationController.delegate = self
         authorizationController.presentationContextProvider = self
         authorizationController.performRequests()
-    }
-    
-    private func randomNonceString(length: Int = 32) -> String {
-        let charset: Array<Character> =
-            Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
-        var result = ""
-        var remainingLength = length
-        
-        while remainingLength > 0 {
-            let randoms: [UInt8] = (0 ..< 16).map { _ in
-                var random: UInt8 = 0
-                let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
-                if errorCode != errSecSuccess {
-                    fatalError(
-                        "Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)"
-                    )
-                }
-                return random
-            }
-            
-            randoms.forEach { random in
-                if remainingLength == 0 {
-                    return
-                }
-                
-                if random < charset.count {
-                    result.append(charset[Int(random)])
-                    remainingLength -= 1
-                }
-            }
-        }
-        
-        return result
-    }
-    
-    @available(iOS 13, *)
-    private func sha256(_ input: String) -> String {
-        let inputData = Data(input.utf8)
-        let hashedData = SHA256.hash(data: inputData)
-        let hashString = hashedData.compactMap {
-            return String(format: "%02x", $0)
-        }.joined()
-        
-        return hashString
     }
 }
 
