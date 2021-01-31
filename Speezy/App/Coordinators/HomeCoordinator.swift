4cb16ebc5b6a05e6a66a32403bd1f0df9de276f3
Matt Beaney <beaneyios@gmail.com>
2021-01-31 13:49:53 +0000

    More UI fixes

--- Speezy/App/Coordinators/HomeCoordinator.swift
+++ Speezy/App/Coordinators/HomeCoordinator.swift
@@ -80,7 +80,7 @@ class HomeCoordinator: ViewCoordinator {
     private class DummyRecordViewController: UIViewController {}
     private func addQuickRecord() {
         let dummyController = DummyRecordViewController()
-        let imageInsets = UIEdgeInsets(top: -10, left: 0, bottom: 5, right: 0)
+        let imageInsets = UIEdgeInsets(top: -5, left: 0, bottom: 1, right: 0)
         
         dummyController.tabBarItem.image = UIImage(
             named: "speezy-tab-item"
