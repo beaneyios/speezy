db839f2bb038a51d6f67bc197727fc922caa0aef
Matt Beaney <beaneyios@gmail.com>
2021-07-11 11:53:38 +0100

    View work is pretty much done.

--- Speezy/Social/View Controllers/PlaybackViewController.swift
+++ Speezy/Social/View Controllers/PlaybackViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 final class PlaybackViewController: UIViewController {
+    enum DrawState {
+        case open
+        case closed
+    }
     
     @IBOutlet weak var imgProfile: UIImageView!
     @IBOutlet weak var playbackSlider: UISlider!
@@ -17,7 +21,11 @@ final class PlaybackViewController: UIViewController {
     @IBOutlet weak var playbackButton: UIButton!
     
     @IBOutlet weak var tagsContainer: UIView!
+    @IBOutlet weak var commentsHandleContainer: UIView!
+    @IBOutlet weak var commentsHandlePosition: NSLayoutConstraint!
     @IBOutlet weak var commentsContainer: UIView!
+    
+    var drawState: DrawState = .closed
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,9 +49,7 @@ final class PlaybackViewController: UIViewController {
             maker.leading.equalToSuperview().offset(16.0)
             maker.trailing.equalToSuperview().offset(-16.0)
         }
-        
-        tagsView.backgroundColor = .red
-        
+                
         tagsView.configure(
             with: tags,
             foreColor: UIColor(named: "speezy-purple")!,
@@ -54,27 +60,110 @@ final class PlaybackViewController: UIViewController {
     }
     
     private func configureCommentsContainer() {
+        configureCommentsViewController()
+        configureCommentsContainerGestures()
+    }
+    
+    private func configureCommentsViewController() {
         let storyboard = UIStoryboard(name: "Social", bundle: nil)
         
-        let viewController = storyboard.instantiateViewController(identifier: "CommentsViewController")
+        let viewController = storyboard.instantiateViewController(
+            identifier: "CommentsViewController"
+        )
         viewController.willMove(toParent: self)
         commentsContainer.addSubview(viewController.view)
         viewController.view.snp.makeConstraints { maker in
             maker.edges.equalToSuperview()
         }
         addChild(viewController)
-        commentsContainer.addShadow(
+        commentsHandleContainer.addShadow(
             opacity: 0.1,
             radius: 10.0,
             offset: CGSize(width: 0.0, height: -10.0)
         )
-        [commentsContainer, viewController.view].forEach {
-            $0?.layer.cornerRadius = 25.0
-            $0?.layer.maskedCorners = [
-                .layerMinXMinYCorner,
-                .layerMaxXMinYCorner
-            ]
-        }
         
+        commentsHandleContainer.layer.cornerRadius = 25.0
+        commentsHandleContainer.layer.maskedCorners = [
+            .layerMinXMinYCorner,
+            .layerMaxXMinYCorner
+        ]
+    }
+    
+    private func configureCommentsContainerGestures() {
+        let pan = UIPanGestureRecognizer(
+            target: self,
+            action: #selector(commentsHandlePanned(sender:))
+        )
+        commentsHandleContainer.addGestureRecognizer(pan)
+        
+        let tap = UITapGestureRecognizer(
+            target: self,
+            action: #selector(commentsHandleTapped(sender:))
+        )
+        commentsHandleContainer.addGestureRecognizer(tap)
+    }
+}
+
+extension PlaybackViewController {
+    private var drawClosedPosition: CGFloat { 118.0 }
+    private var drawOpenPosition: CGFloat { view.frame.height - 150.0 }
+    private var openThreshhold: CGFloat { -125.0 }
+    
+    private var bottomBounceThreshold: CGFloat { 110.0 }
+    private var topBounceThreshold: CGFloat { view.frame.height - 140.0 }
+        
+    private var basePosition: CGFloat {
+        switch drawState {
+        case .open: return drawOpenPosition
+        case .closed: return drawClosedPosition
+        }
+    }
+    
+    private var canMove: Bool {
+        let constant = commentsHandlePosition.constant
+        return constant >= bottomBounceThreshold && constant <= topBounceThreshold
+    }
+    
+    @objc private func commentsHandleTapped(sender: UITapGestureRecognizer) {
+        switch drawState {
+        case .open: closeComments()
+        case .closed: openComments()
+        }
+    }
+    
+    @objc private func commentsHandlePanned(sender: UIPanGestureRecognizer) {
+        let translation = sender.translation(in: view)
+        
+        switch sender.state {
+        case .changed:
+            if canMove {
+                commentsHandlePosition.constant = basePosition - translation.y
+                view.layoutIfNeeded()
+            }
+        case .ended:
+            if translation.y < openThreshhold {
+                openComments()
+            } else {
+                closeComments()
+            }
+        default:
+            break
+        }
+    }
+    
+    private func openComments() {
+        commentsHandlePosition.constant = drawOpenPosition
+        drawState = .open
+        UIView.animate(withDuration: 0.3) {
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    private func closeComments() {
+        commentsHandlePosition.constant = drawClosedPosition
+        drawState = .closed
+        UIView.animate(withDuration: 0.3) {
+            self.view.layoutIfNeeded()
+        }
     }
 }
